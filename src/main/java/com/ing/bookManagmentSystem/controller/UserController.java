341ad8ff49e3089bcc7a2622ef16cdf1b4e0bb60
user1 <user1@LP-CNU4129QG2.HCLT.CORP.HCL.IN>
2019-10-16 17:20:14 +0530

    registration

--- src/main/java/com/ing/bookManagmentSystem/controller/UserController.java
+++ src/main/java/com/ing/bookManagmentSystem/controller/UserController.java
@@ -3,23 +3,38 @@ package com.ing.bookManagmentSystem.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ing.bookManagmentSystem.dto.RegisterDto;
 import com.ing.bookManagmentSystem.dto.RegisterResponseDto;
 import com.ing.bookManagmentSystem.service.UserService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
+@CrossOrigin(allowedHeaders = { "*", "*/" }, origins = { "*", "*/" })
 public class UserController {
 
 	
 	@Autowired
 	UserService userService;
 	
+	
+	/**
+	 * 
+	 * @param registerDto
+	 * @apiNote register for the user
+	 * @author Sharath
+	 * @return register successfull or exists 
+	 */
 	@PostMapping("/register")
-	public ResponseEntity<RegisterResponseDto> register(RegisterDto registerDto)
+	public ResponseEntity<RegisterResponseDto> register(@RequestBody RegisterDto registerDto)
 	{
+		log.info("event for register controller called");
 		return new ResponseEntity(userService.register(registerDto),HttpStatus.OK);
 	}
 	
--- src/main/java/com/ing/bookManagmentSystem/service/UserServiceImpl.java
+++ src/main/java/com/ing/bookManagmentSystem/service/UserServiceImpl.java
@@ -15,36 +15,45 @@ import com.ing.bookManagmentSystem.exception.UserExistException;
 import com.ing.bookManagmentSystem.repository.UserRepository;
 import com.ing.bookManagmentSystem.util.ExceptionConstants;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userRepository;
 	
+	/**
+	 *@author Sharath
+	 *@apiNote service for saving user details
+	 *@return user registered successfully
+	 *
+	 */
 	@Override
 	public RegisterResponseDto register(RegisterDto registerDto) {
-		// TODO Auto-generated method stub
+		
+		log.info("event for user registration service called");
 		
 		User user = new User();
 		RegisterResponseDto response = new RegisterResponseDto();
 		
 		Optional<User> userData= userRepository.findByemailId(registerDto.getEmailId());
-		/*userData.ifPresent(() -> 
-		throw new ResponseError("", HttpStatus.FOUND.value());
-				);*/
 		
-		if(userData.isPresent())
+		userData.ifPresent(data ->
 		{
+			log.info("user service called and user exists");
 			throw new UserExistException(ExceptionConstants.USER_EXISTS, HttpStatus.FOUND.value());
-		}else {
-		
+		});
+			
+			log.info("user service for registartion successfully");
 			BeanUtils.copyProperties(registerDto, user);
 			userRepository.save(user);
 			
 			response.setMessage(ExceptionConstants.USER_CREATION);
 			response.setStatusCode(HttpStatus.OK.value());
 			return response;
-		}
+		
 	}
 
 }
